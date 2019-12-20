4a8fbd7b547296fc9d78a4cdf2d7bdface4a3cc8
Austin Riedl <50588889+adriedl@users.noreply.github.com>
2019-12-20 07:51:58 -0600

    Hello Dashboard with Graph
    Function returns a simple graph of iris data. For proof of concept only.

--- R/HelloDashboard.R
+++ R/HelloDashboard.R
@@ -1,6 +1,13 @@
 # prints the string "Hello, Dashboard"
+#install.packages("ggvis")
+#require(ggvis)
+#library(ggvis)
+
 helloDashboard <- function(data, params){
-  print("Hello, Dashboard!")
+  #print("Hello, Dashboard!")
+  iris %>%
+    ggvis(x=~Sepal.Length, y=~Sepal.Width) %>%
+      layer_points()
 }
 
 #helloDashboard() # hello dashboard function call
